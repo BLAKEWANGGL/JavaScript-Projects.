78663e79805c01aa845c3c0b746ec1675dcbfc5c
BLAKEWANGGL <133913809+BLAKEWANGGL@users.noreply.github.com>
2023-06-30 15:43:11 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var myString = "Hello";
+    myString += " World!";
+    var element = document.getElementById("myParagraph");
+    element.textContent = myString;    
+}
