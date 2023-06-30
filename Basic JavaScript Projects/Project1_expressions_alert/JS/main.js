362e6cb03f2f82d46d08318a86f9bf8c030dd0be
BLAKEWANGGL <133913809+BLAKEWANGGL@users.noreply.github.com>
2023-06-30 11:33:12 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,47 @@
+window.alert("Hello, World!");
+document.write('Hello, world!');
+
+var A = "This is a string";
+document.write(A);
+
+var String = "This is my string!";
+window.alert(String);
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+
+
+document. write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+
+3 + 3
+document.write(3 + 3);
+
+
+function My_First_Function() {              //Defining a function and naming it
+    var str = "This is the button text!";   //Defining a variable and giving ita string value
+
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value
+                                            //of the variable into the HTML elementFromPoint
+                                            //with the "Button_Text" id
+}
+
+
+
+// Define two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the Sent1 and Sent2 variables
+var concatenatedSentence = Sent1 + Sent2;
+
+// Display an alert box with the concatenated sentence
+window.alert(concatenatedSentence);
+
+// Write the concatenated sentence to the document
+document.write(concatenatedSentence);
