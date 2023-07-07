4e054f349e458bc83f49ec358b65670bb6acbf2f
BLAKEWANGGL <133913809+BLAKEWANGGL@users.noreply.github.com>
2023-07-07 12:17:16 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,24 @@
+//main.js
+
+//Assigning local and global variables
+var x = 10; //global variable
+
+function Add_numbers_1() {
+    var x = 99; //local variable
+    document.write(10 + x + "<br>"); //using local variable
+}
+
+function Add_numbers_2() {
+    document.write(x + 11); //using global variable
+    console.log(x); //using console.log to debug which variable is being used
+}
+
+Add_numbers_1();
+Add_numbers_2();
+
+//A function that includes an if statement
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "It is currently before 6pm.";
+    }
+}
